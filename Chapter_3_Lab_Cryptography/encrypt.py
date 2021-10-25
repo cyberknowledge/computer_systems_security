d8ca38adb98065636f8799d5a1df6e4fbb3aaf11
Anon <ctdd@gmail.com>
2021-10-25 20:37:56 +0000

    Chapter 3 Lab

---
+++ Chapter_3_Lab_Cryptography/encrypt.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# A python function for Caesar Cipher - Encrypt
+def encrypt(text,s):
+    result = ""
+    
+    # Read the plain text
+    for i in range(0,len(text)):
+        char = text[i]
+
+        # Encrypt uppercase characters in plain text
+        if (char.isupper()):
+            result += chr((ord(char) + s-65) % 26 + 65)
+        
+        # Encrypt lowercase characters in plain text
+        else:
+            result += chr((ord(char) + s - 97) % 26 + 97)
+        
+    return result
---
+++ Chapter_3_Lab_Cryptography/one_time_pad.py
@@ -0,0 +1,3 @@
+def xor_bytes(key_stream, message):
+    length = len(message)
+    return bytes([key_stream[i] ^ message[i] for i in range(length)])
